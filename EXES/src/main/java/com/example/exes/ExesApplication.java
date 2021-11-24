8996d8a4fdc8d19a7848776caee16df36fffebf1
hayato <hayato@192.168.3.6>
2021-11-24 22:24:02 +0900

    test

---
+++ EXES/src/main/java/com/example/exes/ExesApplication.java
@@ -0,0 +1,13 @@
+package com.example.exes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExesApplication.class, args);
+	}
+
+}
---
+++ EXES/src/test/java/com/example/exes/ExesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
